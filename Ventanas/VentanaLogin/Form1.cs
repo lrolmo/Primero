e10f11d5fcd584a9489b90582352d9b2ad872629
Lroldan <linroldan73@gmail.com>
2019-05-28 10:43:04 -0600

    Ejercicio

---
+++ Ventanas/VentanaLogin/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VentanaLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "LRM" && textBox2.Text == "1234")
+            {
+                //MessageBox.Show("..ACCESSO CORRECTO..");
+                this.Hide();
+                Principal nuevaventana = new Principal();
+                //ventana2 nuevaventana = new ventana2();
+                nuevaventana.Show();
+            }
+            else
+            {
+                MessageBox.Show("..ERROR..Usuario o contraseña incorrecta");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox1.Focus();
+
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Ventanas/VentanaLogin/ventana2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VentanaLogin
+{
+    public partial class ventana2 : Form
+    {
+        public ventana2()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int contador = 0;
+            string elegido;
+
+            if (checkBox1.Checked == true)
+            {
+                contador++;
+            }
+            if (checkBox2.Checked == true)
+            {
+                contador++;
+            }
+            if (radioButton1.Checked== true)
+            {
+                elegido = "Tarjeta de Crédito";
+            }
+            else
+            {
+                elegido = "PayPal";
+            }
+            MessageBox.Show("Ha seleccionado  "+contador+" y cancela con "+elegido);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 ventanalogin = new Form1();
+            ventanalogin.Show();
+        }
+    }
+}
